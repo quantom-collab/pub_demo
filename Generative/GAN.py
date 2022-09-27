ab37a8b43fe5cfe53496a85c7877cded48b34f81
Kishansingh Rajput <kishansingh0817@gmail.com>
2022-09-27 14:31:25 -0400

    GAN with gradient tape

--- Generative/GAN.py
+++ Generative/GAN.py
@@ -40,8 +40,19 @@ class GAN():
         self.events_shape = events_shape
         self.params_shape = params_shape
         self.train_events = train_events
+        lr_schedule_g = tf.keras.optimizers.schedules.ExponentialDecay(
+            1e-4,
+            decay_steps=10000,
+            decay_rate=0.8,
+            staircase=True)
+        lr_schedule_d = tf.keras.optimizers.schedules.ExponentialDecay(
+            1e-3,
+            decay_steps=10000,
+            decay_rate=0.8,
+            staircase=True)
+        # Not using the learning rate scheduler yet, it's work in progress!
         self.d_optimizer = tf.keras.optimizers.Adam(learning_rate)
-        self.g_optimizer = tf.keras.optimizers.Adam(learning_rate/10)
+        self.g_optimizer = tf.keras.optimizers.Adam(learning_rate/10.)
         self.batch_size = batch_size
         self.start_epoch = start_epoch
         self.truth = truth
@@ -50,6 +61,9 @@ class GAN():
         self.parmin = parmin.astype(np.float32) 
         self.parmax = parmax.astype(np.float32)
         self.output_dir = output_directory
+        self.d_loss_function = tf.keras.losses.MeanSquaredError()
+        self.g_loss_function = tf.keras.losses.MeanSquaredError()
+
         
         self.discriminator = self.build_discriminator()
         self.generator = self.build_generator()
@@ -64,17 +78,17 @@ class GAN():
             metrics=['accuracy'])
 
         # The generator takes noise as input and generated the events 
-        z = Input(shape=(self.noise_dim,))
-        e, n1, n2 = self.generator(z)
-        print(e.shape)
-        # For the combined model we will only train the generator
-        self.discriminator.trainable = False
-        # The valid takes generated images as input and determines validity
-        valid = self.discriminator(e)
-        # The combined model  (stacked generator and discriminator)
-        # Trains generator to fool discriminator
-        self.combined = Model(z, [valid, n1, n2])
-        self.combined.compile(loss='mse', optimizer=self.g_optimizer)#, loss_weights=[1, 1, 1])
+#         z = Input(shape=(self.noise_dim,))
+#         e, n1, n2 = self.generator(z)
+#         print(e.shape)
+#         # For the combined model we will only train the generator
+#         self.discriminator.trainable = False
+#         # The valid takes generated images as input and determines validity
+#         valid = self.discriminator(e)
+#         # The combined model  (stacked generator and discriminator)
+#         # Trains generator to fool discriminator
+#         self.combined = Model(z, [valid, n1, n2])
+#         self.combined.compile(loss='mse', optimizer=self.g_optimizer)#, loss_weights=[1, 1, 1])
 
     def build_generator(self):
         "Build the generator model"
@@ -93,7 +107,7 @@ class GAN():
 
         output = Dense(self.params_shape, activation="sigmoid")(hidden)
 
-        output = Lambda(lambda x: wrapperGenerator(x, self.parmin, self.parmax, 1))(output)
+#         output = Lambda(lambda x: wrapperGenerator(x, self.parmin, self.parmax, 1))(output)
         generator = Model(inputs=inputs, outputs=output)
         generator.summary()
         events = generator(inputs)
@@ -126,28 +140,42 @@ class GAN():
         return model
 
     def train(self, epochs, sample_interval=50):
-
-        fake = np.zeros((self.batch_size, 1)) 
+        
+        N = 200
+        fake = tf.zeros((self.batch_size, 1)) 
         valid = tf.ones((self.batch_size, 1))
+#         fake_master = tf.zeros((self.batch_size*N, 1)) 
+#         valid_master = tf.ones((self.batch_size*N, 1)) 
         
         for epoch in range(self.start_epoch, epochs):
             
             indices = np.random.randint(0, self.train_events.shape[1], self.batch_size)
             evnts = np.transpose(self.train_events[:, indices])
-
             noise = tf.random.normal((self.batch_size, self.noise_dim), 0.5, 0.3)
+            
+            with tf.GradientTape(persistent=True) as tape:
+                params = self.generator(noise)
+                events, norm1, norm2 = wrapperGenerator(params, self.parmin, self.parmax, 1)
 
-            events, norm1, norm2 = self.generator.predict_on_batch(noise)
-            # Train the discriminator
-            d_loss_real = self.discriminator.train_on_batch(evnts, valid)
-            d_loss_fake = self.discriminator.train_on_batch(events, fake)
-            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-
-            g_loss = self.combined.train_on_batch(noise, [valid, self.train_norms1, self.train_norms2])
+                real_pred = self.discriminator(evnts)
+                d_loss_real = self.d_loss_function(valid, real_pred)
+                g_loss_real = self.g_loss_function(valid, real_pred)
                 
+                fake_pred = self.discriminator(events)
+                d_loss_fake = self.d_loss_function(fake, fake_pred)
+                g_loss_fake = self.g_loss_function(valid, fake_pred)
                 
-            if epoch % 10 == 0:
-                print("At epoch ", epoch, " Norms", tf.reduce_mean(norm1), tf.reduce_mean(norm2), "g_loss: ", g_loss[0], " d_loss: ", d_loss[0])
+                d_loss = 0.5 * tf.add(d_loss_real, d_loss_fake)
+                g_loss = 0.5*tf.add(g_loss_real, g_loss_fake) + tf.losses.mse(self.train_norms1, norm1) + tf.losses.mse(self.train_norms2, norm2)
+                
+            d_grads = tape.gradient(d_loss, self.discriminator.trainable_variables)
+            self.d_optimizer.apply_gradients(zip(d_grads, self.discriminator.trainable_variables))
+            
+            g_grads = tape.gradient(g_loss, self.generator.trainable_variables)
+            self.g_optimizer.apply_gradients(zip(g_grads, self.generator.trainable_variables))
+                
+            if epoch % 100 == 0:
+                print("At epoch ", epoch, " Norms", tf.reduce_mean(norm1), tf.reduce_mean(norm2), "g_loss: ", g_loss, " d_loss: ", d_loss)
 
             if epoch % sample_interval == 0:
                 
@@ -156,8 +184,10 @@ class GAN():
                 self.discriminator.save_weights(os.path.join(self.output_dir, "2022.08.21_discriminator_6Params.h5"))
                 print("models saved...")
                 
-                num = 100000
-                gen_events, norm1, norm2 = self.generate(num)
+                num = 10000
+                gen_params = self.generate(num)
+                gen_events, norm1, norm2 = wrapperGenerator(gen_params, self.parmin, self.parmax, 1)
+                gen_events = gen_events.numpy()
                 print("Events generated...")
                 print("training norms", self.train_norms1[0], self.train_norms2[0])
                 plt.clf()
@@ -175,16 +205,7 @@ class GAN():
                 plt.savefig(os.path.join(self.output_dir, "Events_"+epoch_str+".png"))
                 plt.show()
                 
-                get_1st_layer_output = K.function([self.generator.layers[0].input],
-                                  [self.generator.layers[1].layers[9].output])
-        
-                layer_output = []
-                
-                noise = tf.random.normal((100000, self.noise_dim), 0.5, 0.2)
-                p = get_1st_layer_output(noise)
-                layer_output = p[0]
-
-                results = layer_output
+                results = gen_params.numpy()
                 print(results.shape)
                 plt.clf()
                 plt.figure(figsize=(20,15))
@@ -203,6 +224,5 @@ class GAN():
             n = self.train_events.shape[1]
         print("Generating: ", n, " events...")
         input_noise = tf.random.normal((n, self.noise_dim), 0.5, 0.2)
-        events, norm1, norm2 = self.generator(input_noise)
-        print("Generated Norms: ", norm1[:10], norm2[:10])
-        return events.numpy(), norm1.numpy(), norm2.numpy()
+        params = self.generator(input_noise)
+        return params
